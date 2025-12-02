e5de1791eef3926f4781f049bb837ac53dcfb4d1
M Farhan Ramadhan <frhn.r3@gmail.com>
2025-12-02 12:12:26 +0700

    test pre-hook #3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,16 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [...nextTypescript, ...nextCoreWebVitals];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}, ...compat.extends("next/core-web-vitals", "next/typescript")];
+
+export default eslintConfig;
