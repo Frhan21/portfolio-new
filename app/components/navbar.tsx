c9078109db7b5a77c86024795e27a88639e43aa6
frhan21 <frhn.r3@gmail.com>
2026-05-27 16:36:44 +0700

    fix(nav): fix nav issue to associate with defaultThemes by system

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -18,7 +18,7 @@ const navLinks = [
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -79,11 +79,17 @@ export default function Navbar() {
 
             {mounted && (
               <button
-                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                onClick={() =>
+                  setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+                }
                 className="w-10 h-10 flex items-center justify-center rounded-full bg-slate-100 dark:bg-slate-800 text-slate-600 dark:text-slate-300 hover:text-primary transition-colors border border-slate-200 dark:border-slate-700"
                 aria-label="Toggle Dark Mode"
               >
-                {theme === 'dark' ? <Sun size={18} /> : <Moon size={18} />}
+                {resolvedTheme === 'dark' ? (
+                  <Sun size={18} />
+                ) : (
+                  <Moon size={18} />
+                )}
               </button>
             )}
           </div>
@@ -92,10 +98,16 @@ export default function Navbar() {
           <div className="lg:hidden flex items-center space-x-4">
             {mounted && (
               <button
-                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                onClick={() =>
+                  setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+                }
                 className="w-9 h-9 flex items-center justify-center rounded-full bg-slate-100 dark:bg-slate-800 text-slate-600 dark:text-slate-300"
               >
-                {theme === 'dark' ? <Sun size={18} /> : <Moon size={18} />}
+                {resolvedTheme === 'dark' ? (
+                  <Sun size={18} />
+                ) : (
+                  <Moon size={18} />
+                )}
               </button>
             )}
             <button
