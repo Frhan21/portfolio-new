adcccfb32bf831e8a4ae7cdf0610021d7e837a6c
frhan21 <frhn.r3@gmail.com>
2026-06-08 14:22:17 +0700

    feat(experience): add experience section on database and implement to CMS and landing page

--- components/ui/date-picker.tsx
+++ components/ui/date-picker.tsx
@@ -52,12 +52,7 @@ export function DatePicker({
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-        />
+        <Calendar mode="single" selected={date} onSelect={setDate} />
       </PopoverContent>
     </Popover>
   );
